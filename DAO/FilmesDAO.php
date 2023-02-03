2f000a90318cbeb53190588cadf99513248c955c
Any97Cris <criscianysilva1997@gmail.com>
2023-02-03 08:59:32 -0400

    import aqruivo model

--- DAO/FilmesDAO.php
+++ DAO/FilmesDAO.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once "Model/Filmes.php";
+
 class FilmesDAO {
     private $pdo;
 
@@ -8,7 +10,7 @@ class FilmesDAO {
     }
 
     public function add(Filmes $f){
-        
+
     }
 }
 
