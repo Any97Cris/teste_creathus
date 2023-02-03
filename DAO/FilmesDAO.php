57d9385a1efd855f24d6ea1b9a38b62231201840
Any97Cris <criscianysilva1997@gmail.com>
2023-02-03 10:19:53 -0400

    metodo add

--- DAO/FilmesDAO.php
+++ DAO/FilmesDAO.php
@@ -10,7 +10,15 @@ class FilmesDAO {
     }
 
     public function add(Filmes $f){
+        $sql = $this->pdo->prepare("INSERT INTO filmes(filme_titulo,filme_autor,filme_descricao,filme_ano) VALUES(:titulo,:autor,:descricao,:ano)");
+        $sql->bindValue(':titulo', $f->getTitulo());
+        $sql->bindValue(':autor', $f->getAutor());
+        $sql->bindValue(':descricao', $f->getDescricao());
+        $sql->bindValue(':ano', $f->getAno());
+        $sql->execute();
 
+        $f->setId($this->pdo->lastInsertId());
+        return $f;
     }
 }
 
