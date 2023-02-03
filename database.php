2f4b296246faeb2805e2a86a7156848560ac1290
Any97Cris <criscianysilva1997@gmail.com>
2023-02-03 08:49:08 -0400

    comunicacao com banco

--- database.php
+++ database.php
@@ -1,5 +1,15 @@
 <?php
 
+try{
 
+$db = "mysql:dbname=entretenimentos;host=127.0.0.1";
+$dbuser = "root";
+$dbpassword = "";
+
+$pdo = new PDO($db, $dbuser,$dbpassword);
+
+}catch(Exception $e){
+    echo $e->getMessage();
+}
 
 ?>
