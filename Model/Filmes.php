f21ecd86b5a4780140aaef0cfd03860f9a9b1555
Any97Cris <criscianysilva1997@gmail.com>
2023-02-03 08:55:51 -0400

    clase com get e set

---
+++ Model/Filmes.php
@@ -0,0 +1,47 @@
+<?php
+
+class Filmes {
+    private $id;
+    private $titulo;
+    private $autor;
+    private $descricao;
+    private $ano;
+
+    public function getId(){
+        return $this->id;
+    }
+    public function setId($id){
+        $this->id = $id;
+    }
+
+    public function getTitulo(){
+        return $this->titulo;
+    }
+    public function setTitulo($titulo){
+        $this->titulo = $titulo;
+    }
+
+    public function getAutor(){
+        return $this->autor;
+    }
+    public function setAutor($autor){
+        $this->autor = $autor;
+    }
+
+    public function getDescricao(){
+        return $this->descricao;
+    }
+    public function setDescricao($descricao){
+        $this->descricao = $descricao;
+    }
+
+    public function getAno(){
+        return $this->ano;
+    }
+    public function setAno($ano){
+        $this->ano = $ano;
+    }
+
+}
+
+?>
