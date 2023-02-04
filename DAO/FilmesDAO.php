0c0efff834b31e03a6a9804ab533831250e3ab1b
Any97Cris <criscianysilva1997@gmail.com>
2023-02-04 08:47:59 -0400

    listar dados

--- DAO/FilmesDAO.php
+++ DAO/FilmesDAO.php
@@ -20,6 +20,29 @@ class FilmesDAO {
         $f->setId($this->pdo->lastInsertId());
         return $f;
     }
+
+    public function findAll(){
+        $array = [];
+        $sql = $this->pdo->query("SELECT * FROM filmes");
+
+        if($sql->rowCount() > 0){
+            $dados = $sql->fetchAll();
+
+            foreach($dados as $item){
+                $data = new Filmes();
+                $data->setId($item['filme_id']);
+                $data->setTitulo($item['filme_titulo']);
+                $data->setAutor($item['filme_autor']);
+                $data->setDescricao($item['filme_descricao']);
+                $data->setAno($item['filme_ano']);
+
+            }
+            
+            $array = $data;
+        }
+
+        return $array;
+    }
 }
 
 ?>
